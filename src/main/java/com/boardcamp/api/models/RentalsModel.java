13eee081cc764e29cf1c7b6bfbd1df4d7d30c34f
Lucas Macedo <lucasamacedo@hotmail.com>
2024-01-31 20:03:43 -0300

    feat: created rentals model

---
+++ src/main/java/com/boardcamp/api/models/RentalsModel.java
@@ -0,0 +1,49 @@
+package com.boardcamp.api.models;
+
+import java.time.LocalDate;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data              
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name = "rentals")
+public class RentalsModel {
+  @Id
+  @GeneratedValue(strategy = GenerationType.AUTO)
+  private Long id;
+
+  @ManyToOne
+  @JoinColumn(name = "customerId", referencedColumnName = "id", nullable = false)
+  private CustomersModel customer;
+
+  @ManyToOne
+  @JoinColumn(name = "gameId", referencedColumnName = "id", nullable = false)
+  private GamesModel game;
+
+  @Column(nullable = false)
+  private LocalDate rentDate;
+
+  @Column(nullable = false)
+  private Long daysRented;
+
+  @Column(nullable = true)
+  private LocalDate returnDate;
+
+  @Column(nullable = false)
+  private Long originalPrice;
+
+  @Column(columnDefinition = "bigint default 0")
+  private Long delayFee;
+}
