1815d7f89a322491f0c303c17b9ed2d4c4db1d68
Lucas Macedo <lucasamacedo@hotmail.com>
2024-02-05 18:16:11 -0300

    feat: created an endpoint for get at game controller

--- src/main/java/com/boardcamp/api/controllers/GameController.java
+++ src/main/java/com/boardcamp/api/controllers/GameController.java
@@ -7,12 +7,15 @@ import com.boardcamp.api.dtos.GameDTO;
 import com.boardcamp.api.models.GameModel;
 import com.boardcamp.api.services.GameService;
 
+import java.util.List;
+
 import jakarta.validation.Valid;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.GetMapping;
 
 
 
@@ -31,5 +34,13 @@ public class GameController {
         
         return ResponseEntity.status(HttpStatus.CREATED).body(game);
     }
+
+    @GetMapping
+    public ResponseEntity<List<GameModel>> getGames() {
+        List<GameModel> game = gameService.findAll();
+
+        return ResponseEntity.status(HttpStatus.OK).body(game);
+    }
+    
     
 }
