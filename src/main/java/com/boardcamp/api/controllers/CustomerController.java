721fdfcadf310cae05306e0defaad728e16ab654
Lucas Macedo <lucasamacedo@hotmail.com>
2024-02-06 00:01:27 -0300

    feat: created customer controller and the endpoint post

---
+++ src/main/java/com/boardcamp/api/controllers/CustomerController.java
@@ -0,0 +1,35 @@
+package com.boardcamp.api.controllers;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.boardcamp.api.dtos.CustomerDTO;
+import com.boardcamp.api.models.CustomerModel;
+import com.boardcamp.api.services.CustomerService;
+
+import jakarta.validation.Valid;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+
+
+
+@RestController
+@RequestMapping("/customers")
+public class CustomerController {
+    final CustomerService customerService;
+
+    CustomerController(CustomerService customerService){
+        this.customerService = customerService;
+    }
+
+    @PostMapping
+    public ResponseEntity<CustomerModel> save(@RequestBody @Valid CustomerDTO body) {
+        CustomerModel customer = customerService.save(body);
+        
+        return ResponseEntity.status(HttpStatus.CREATED).body(customer);
+    }
+    
+}
