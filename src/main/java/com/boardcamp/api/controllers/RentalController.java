dffed086946dc42b1e79b59e4ffd617b40897616
Lucas Macedo <lucasamacedo@hotmail.com>
2024-02-06 00:42:38 -0300

    feat: created rental controller and the endpoint post

---
+++ src/main/java/com/boardcamp/api/controllers/RentalController.java
@@ -0,0 +1,38 @@
+package com.boardcamp.api.controllers;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.boardcamp.api.dtos.RentalDTO;
+import com.boardcamp.api.models.RentalModel;
+import com.boardcamp.api.services.RentalService;
+
+import java.util.List;
+
+import jakarta.validation.Valid;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.GetMapping;
+
+
+
+@RestController
+@RequestMapping("/games")
+public class RentalController {
+    final RentalService rentalService;
+
+    RentalController(RentalService rentalService){
+        this.rentalService = rentalService;
+    }
+
+    @PostMapping
+    public ResponseEntity<RentalModel> save(@RequestBody @Valid RentalDTO body) {
+        RentalModel rental = rentalService.save(body);
+        
+        return ResponseEntity.status(HttpStatus.CREATED).body(rental);
+    }
+    
+}
